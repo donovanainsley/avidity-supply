818c0827191dcce8a6d381a36ae1e6c4aef42077
Donovan Ainsley <donovan_weise@live.co.uk>
2024-06-15 13:49:45 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PRwFpH4PeNuMcYvlQifVw3cfcNY0t75Nu2xk3BFUqktl5HQFCfmHm6KIrjTdaRKz4Dxc8kpqu18v4JBKnOzzsWn00THvruf9p',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
