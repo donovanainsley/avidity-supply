a9d55852d0cc7102d214323f23bb88600b3760d4
Donovan Ainsley <donovan_weise@live.co.uk>
2024-06-22 14:38:02 +0000

    Contact - added forms, updated views and migrated models

--- contact/forms.py
+++ contact/forms.py
@@ -1,1 +1,29 @@
 from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('contact_name', 'contact_email', 'contact_message')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders, remove auto-generated
+        labels and set autofocus on first field
+        """
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'contact_name' : 'Full Name', 
+            'contact_email': 'Email Address', 
+            'contact_message': 'Your Message',
+        }
+
+        self.fields['contact_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from .models import Contact
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact_us(request):
     """ A view to render contact form """
-    return render(request, 'contact/contact_us.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact_us.html', {'form': form})
