263e4728458e135e5831fc9e3773cfb2b924cef2
Donovan Ainsley <donovan_weise@live.co.uk>
2024-06-22 12:28:12 +0000

    added contact models and updated admin, views and contact_us.html

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    class Meta:
+        verbose_name_plural = "Contact Messages"
+        
+    contact_name = models.CharField(max_length=200, null=False, blank=False)
+    contact_email = models.EmailField(max_length=200, null=False, blank=False)
+    contact_message = models.TextField(max_length=1000, null=False, blank=False)
+    contact_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.contact_name
